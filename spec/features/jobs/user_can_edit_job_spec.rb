85329395d5a989570ed5f32f52b132c0beda4538
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-08-27 16:52:04 -0600

    Job spec set up, job show feature test passing.

--- spec/features/jobs/user_can_edit_job_spec.rb
+++ spec/features/jobs/user_can_edit_job_spec.rb
@@ -7,13 +7,13 @@ describe 'User edits existing job' do
     job = company.jobs.create(title: "IT Tech", level_of_interest: 3, city: "Denver", category_id: category.id)
     visit jobs_path(job)
     click_link "Edit"
+    
     expect(current_path).to eq(edit_job_path(job))
     fill_in "job[title]", with:"Tech Specialist"
     fill_in "job[city]", with: "Mars"
     fill_in "job[level_of_interest]", with: 5
 
     click_button "Update"
-    #save_and_open_page
     expect(current_path).to eq(jobs_path)
     expect(page).to have_content("Tech Specialist")
     expect(page).to have_content("Mars")
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -6,7 +6,6 @@ describe "User sees a specific job" do
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
 
     visit company_job_path(company, job)
-    save_and_open_page
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")

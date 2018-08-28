4c3d86ac0f78f7e89eff7472be47fe49a6fad8b0
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-08-27 19:10:48 -0600

    Adds drop down menu on new job page for category.

--- spec/features/jobs/user_can_edit_job_spec.rb
+++ spec/features/jobs/user_can_edit_job_spec.rb
@@ -7,12 +7,12 @@ describe 'User edits existing job' do
     job = company.jobs.create(title: "IT Tech", level_of_interest: 3, city: "Denver", category_id: category.id)
     visit jobs_path(job)
     click_link "Edit"
-    
+
     expect(current_path).to eq(edit_job_path(job))
     fill_in "job[title]", with:"Tech Specialist"
     fill_in "job[city]", with: "Mars"
     fill_in "job[level_of_interest]", with: 5
-
+    #save_and_open_page
     click_button "Update"
     expect(current_path).to eq(jobs_path)
     expect(page).to have_content("Tech Specialist")
--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 describe "User creates a new job" do
   scenario "a user can create a new job" do
+    category_1 = Category.create!(title: 'Software Development')
+    category_2 = Category.create!(title: 'Advertising')
     company = Company.create!(name: "ESPN")
     visit new_company_job_path(company)
-
+    save_and_open_page
     fill_in "job[title]", with: "Developer"
     fill_in "job[description]", with: "So fun!"
     fill_in "job[level_of_interest]", with: 80
     fill_in "job[city]", with: "Denver"
 
     click_button "Create Job"
-    
+
     expect(current_path).to eq(jobs_path)
 
     expect(page).to have_content("ESPN")
